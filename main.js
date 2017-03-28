4684d003278cd0f705ae03b6f9d1e72efbb6721c
David Noriega <dnori817@gmail.com>
2017-03-27 23:08:20 -0400

    initial commit; challenge 1 & 2 complete

---
+++ main.js
@@ -0,0 +1,28 @@
+//1. Multi-dimensional Array
+
+var nesGames = ["Final Fantasy", "Castlevania", "Contra"];
+
+var superNesGames = ["Super Mario World", "Super Metroid", "Final Fantasy II"];
+
+var nintendoConsoles = [nesGames, superNesGames];
+
+//outputs the two Final Fantasy games
+console.log(nintendoConsoles[0][0]);
+console.log(nintendoConsoles[1][2]);
+
+
+
+//2. Compliment Function
+
+function complimentPerson(name) {
+	
+	return name + " is awesome!!!";
+}
+
+console.log(complimentPerson("Dave"));
+console.log(complimentPerson("Damien"));
+console.log(complimentPerson("Danielle"));
+
+
+
+//3. 99 Bottles of Beer
